b89208779c6dd834972863d3b0db4eaf3184298d
lmdxnmx <nikita021103@mail.ru>
2024-02-15 11:25:10 +0300

    fix logs

--- IoMT.SDK/DataBase/Logs+CoreDataClass.swift
+++ IoMT.SDK/DataBase/Logs+CoreDataClass.swift
@@ -1,15 +1,16 @@
+
 //
-//  Logs+CoreDataClass.swift
+//  Entity+CoreDataClass.swift
 //  IoMT.SDK
 //
-//  Created by Никита on 15.02.2024.
+//  Created by Никита on 13.02.2024.
 //
 //
 
 import Foundation
 import CoreData
 
-@objc(Logs)
+
 public class Logs: NSManagedObject {
 
 }
