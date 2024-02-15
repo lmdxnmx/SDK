74546c9cb6ad4e5117d4a3ddb7e5e14f9ac3db4d
lmdxnmx <nikita021103@mail.ru>
2024-02-15 15:32:24 +0300

    fix

--- IoMT.SDK/InternetManager/InternetManager.swift
+++ IoMT.SDK/InternetManager/InternetManager.swift
@@ -33,14 +33,7 @@ fileprivate class _baseCallback: DeviceCallback {
     
     static internal func getManager () -> InternetManager {
         if sharedManager == nil {
-            if let storedUUIDString = UserDefaults.standard.string(forKey: "instanceId"),
-               let storedUUID = UUID(uuidString: storedUUIDString) {
-                   sharedManager = InternetManager(login: "", password: "", debug: true, callback: _baseCallback(),instanceId:storedUUID)
-            } else {
-                let newUUID = UUID()
-                UserDefaults.standard.set(newUUID.uuidString, forKey: "instanceId")
-                sharedManager = InternetManager(login: "", password: "", debug: true, callback: _baseCallback(),instanceId:newUUID)
-            }
+                   sharedManager = InternetManager(login: "", password: "", debug: true, callback: _baseCallback())
         }
         return sharedManager!
     }
