b45f46ed12ea6f69b92fa8af55923b495c16f4ed
lmdxnmx <nikita021103@mail.ru>
2024-02-15 15:15:15 +0300

    add dictionary

--- IoMT.SDK/MainServices/LogService.swift
+++ IoMT.SDK/MainServices/LogService.swift
@@ -46,34 +46,32 @@ import CoreData
     public func removeLogs(){
         
     }
-    public func sendLogs(){
-        let context = CoreDataStack.shared.viewContext
-        let fetchRequest: NSFetchRequest<Logs> = Logs.fetchRequest()
-       
-        do {
-            let logs = try context.fetch(fetchRequest)
-           
-            // Собираем все логи в массив данных
-            var logsDataArray = [[String: Any]]()
-            let dateFormatter = ISO8601DateFormatter()
-            for log in logs {
-                let logData: [String: Any] = [
-                    "date": dateFormatter.string(from: log.date ?? Date()), // Преобразуем дату в строку
-                    "log": log.log ?? ""
-                ]
-                logsDataArray.append(logData)
-            }
-           
-            // Подготовка данных для отправки на сервер
-            let jsonData = try JSONSerialization.data(withJSONObject: logsDataArray, options: [])
-           
-            // Отправка данных на сервер
-            sendLogsToServer(data: jsonData)
-           
-        } catch {
-            print("Ошибка при получении данных из CoreData: \(error)")
-        }
-    }
+     public func sendLogs(){
+         let context = CoreDataStack.shared.viewContext
+         let fetchRequest: NSFetchRequest<Logs> = Logs.fetchRequest()
+        
+         do {
+             let logs = try context.fetch(fetchRequest)
+            
+             // Собираем все логи в словарь данных
+             var logsDataDictionary = [String: Any]()
+             let dateFormatter = ISO8601DateFormatter()
+             for log in logs {
+                 let dateString = dateFormatter.string(from: log.date ?? Date()) // Преобразуем дату в строку
+                 logsDataDictionary[dateString] = log.log ?? ""
+             }
+            
+             // Подготовка данных для отправки на сервер
+             let jsonData = try JSONSerialization.data(withJSONObject: logsDataDictionary, options: [])
+            
+             // Отправка данных на сервер
+             sendLogsToServer(data: jsonData)
+            
+         } catch {
+             print("Ошибка при получении данных из CoreData: \(error)")
+         }
+     }
+
 
     
     private func sendLogsToServer(data: Data) {
