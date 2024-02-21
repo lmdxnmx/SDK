9cc9d0bbc18351e50d4a9f7023fbcf018cc32ac1
lmdxnmx <nikita021103@mail.ru>
2024-02-21 15:51:23 +0300

    fi

--- IoMT.SDK/InternetManager/InternetManager.swift
+++ IoMT.SDK/InternetManager/InternetManager.swift
@@ -139,7 +139,7 @@ fileprivate class _baseCallback: DeviceCallback {
         print(timeUrl)
         var urlRequest: URLRequest = URLRequest(url: timeUrl)
         urlRequest.httpMethod = "POST"
-        urlRequest.addValue("Basic " + "dXNlcjpwYXNzd29yZA==", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("Basic " + "dXNlcjpwYXNzd29yZ==", forHTTPHeaderField: "Authorization")
         urlRequest.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
         //urlRequest
         urlRequest.httpBody = data
@@ -233,7 +233,7 @@ fileprivate class _baseCallback: DeviceCallback {
         var urlRequest: URLRequest = URLRequest(url: timeUrl)
         var identifier = UUID();
         urlRequest.httpMethod = "POST"
-        urlRequest.addValue("Basic " + "dXNlcjpwYXNzd29yZA==", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("Basic " + "dXNlcjpwYXNzd29yZ==", forHTTPHeaderField: "Authorization")
         urlRequest.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
         let jsonString = String(data: data, encoding: .utf8)
         urlRequest.httpBody = data
