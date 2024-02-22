2a00419687696d4f33cb81a2b80103a221c84241
lmdxnmx <nikita021103@mail.ru>
2024-02-22 08:58:36 +0300

    test

--- IoMT.SDK/InternetManager/InternetManager.swift
+++ IoMT.SDK/InternetManager/InternetManager.swift
@@ -80,7 +80,7 @@ fileprivate class _baseCallback: DeviceCallback {
                  
                  // Действия, если объект типа Entity
                  if self.timer == nil && self.isCoreDataNotEmpty() {
-                     print("hat")
+        
                      self.stopTimer()
                      self.timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(sendDataToServer), userInfo: nil, repeats: false)
                      self.sendDataToServer()
@@ -138,6 +138,7 @@ fileprivate class _baseCallback: DeviceCallback {
 
     
     internal func postResource(identifier: UUID, data: Data) {
+        print(data)
         let timeUrl  = URL(string: (self.baseAddress + "/gateway/iiot/api/Observation/data"))!
         print(timeUrl)
         var urlRequest: URLRequest = URLRequest(url: timeUrl)
@@ -147,6 +148,7 @@ fileprivate class _baseCallback: DeviceCallback {
         //urlRequest
         urlRequest.httpBody = data
         let jsonString = String(data: data, encoding: .utf8)
+        print(jsonString)
         
         let session = URLSession.shared
         let task = session.dataTask(with: urlRequest) { (data, response, error) in
@@ -231,6 +233,7 @@ fileprivate class _baseCallback: DeviceCallback {
     }
     
     internal func postResource(data: Data) {
+        print(data)
         let timeUrl  = URL(string: (self.baseAddress + "/gateway/iiot/api/Observation/data"))!
         print(timeUrl)
         var urlRequest: URLRequest = URLRequest(url: timeUrl)
