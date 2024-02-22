ea8c841ffefe1d4a25fc080436908b9c618f9511
lmdxnmx <nikita021103@mail.ru>
2024-02-22 12:50:33 +0300

    fix

--- IoMT.SDK/MainServices/BundleTemplate.swift
+++ IoMT.SDK/MainServices/BundleTemplate.swift
@@ -7,7 +7,8 @@ internal class BundleTemplate {
         
         for data in dataArray {
             if let jsonData = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                entryArray.append("{\"resource\":\(jsonData)}")
+                let resourceDict: [String: Any] = ["resource": jsonData]
+                entryArray.append(resourceDict)
             }
         }
         
@@ -19,7 +20,7 @@ internal class BundleTemplate {
         do {
             let jsonData = try JSONSerialization.data(withJSONObject: bundleData, options: [])
             print(String(data: jsonData, encoding: .utf8))
-            DeviceService.getInstance().im.postResource(data: jsonData, bundle:true)
+            DeviceService.getInstance().im.postResource(data: jsonData, bundle: true)
         } catch {
             print("Ошибка кодирования данных в JSON: \(error.localizedDescription)")
         }
