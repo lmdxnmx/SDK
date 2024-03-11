5cb1714dc2ee59ac495ab92dbf5e4529360efeb9
lmdxnmx <nikita021103@mail.ru>
2024-03-11 08:36:41 +0300

    fix timer bug on prod

--- IoMT.SDK/InternetManager/InternetManager.swift
+++ IoMT.SDK/InternetManager/InternetManager.swift
@@ -192,7 +192,7 @@ fileprivate class _baseCallback: DeviceCallback {
                         }}}catch{
                             DeviceService.getInstance().ls.addLogs(text:"Ошибка сохранения: \(error.localizedDescription)")
                     }
-           
+                self.increaseInterval()
                 return
             }
             if let httpResponse = response as? HTTPURLResponse {
@@ -229,6 +229,7 @@ fileprivate class _baseCallback: DeviceCallback {
                             }
                 }
                     self.callback.onSendData(mac: identifier, status: PlatformStatus.Failed)
+                    self.increaseInterval()
                 }
             }
             if let responseData = data {
@@ -274,7 +275,7 @@ fileprivate class _baseCallback: DeviceCallback {
                         }}}catch{
                         DeviceService.getInstance().ls.addLogs(text:"Ошибка сохранения: \(error.localizedDescription)")
                     }
-     
+                self.increaseInterval()
                 return
             }
             if let httpResponse = response as? HTTPURLResponse {
@@ -307,6 +308,7 @@ fileprivate class _baseCallback: DeviceCallback {
                             }
                 }
                     self.callback.onSendData(mac: identifier, status: PlatformStatus.Failed)
+                    self.increaseInterval()
                 }
             }
             if let responseData = data {
@@ -335,6 +337,7 @@ fileprivate class _baseCallback: DeviceCallback {
              if let error = error {
                  self.callback.onExpection(mac: identifier, ex: error)
                  DeviceService.getInstance().ls.addLogs(text:"Error: \(error)")
+                 self.increaseInterval()
             
              }
              if let httpResponse = response as? HTTPURLResponse {
@@ -373,6 +376,7 @@ fileprivate class _baseCallback: DeviceCallback {
                  }
                  else{
                      self.callback.onSendData(mac: identifier, status: PlatformStatus.Failed)
+                     self.increaseInterval()
                  }
              }
              if let responseData = data {
@@ -473,7 +477,7 @@ fileprivate class _baseCallback: DeviceCallback {
                  } catch {
                      DeviceService.getInstance().ls.addLogs(text: "Ошибка при получении объектов из Core Data: \(error)")
                  }
-                 self.increaseInterval()
+                 
              }else{
                  self.stopTimer()
                  self.interval = 1;
