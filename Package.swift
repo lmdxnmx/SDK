72319f2d120baf9d4c0d93895fa9903e68280853
lmdxnmx <nikita021103@mail.ru>
2024-03-27 10:25:33 +0300

    fix reach_version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .target(
             name: "IoMT.SDK",
-            dependencies: ["Reachability.swift"],
+            dependencies: ["ReachabilitySwift"],
             path: "IoMT.SDK"
         )
     ],
