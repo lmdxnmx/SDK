a170edfcd295c48bf2780a965a95001835aab9d9
lmdxnmx <nikita021103@mail.ru>
2024-03-27 14:51:37 +0300

    fix

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "IoMT.SDK",
+    platforms: [
+        .iOS("11.0")
+    ],
+    products: [
+        .library(
+            name: "IoMT.SDK",
+            targets: ["IoMT.SDK"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ashleymills/Reachability.swift.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "IoMT.SDK",
+            dependencies: [],
+            path: "IoMT.SDK"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
