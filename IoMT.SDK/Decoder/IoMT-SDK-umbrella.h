8d18416724e3b597ec975c26067ae1b124f4232e
lmdxnmx <nikita021103@mail.ru>
2024-04-01 17:08:15 +0300

    fix

---
+++ IoMT.SDK/Decoder/IoMT-SDK-umbrella.h
@@ -0,0 +1,15 @@
+//
+//  IoMT-SDK-umbrella.h
+//  IoMT.SDK
+//
+//  Created by Никита on 01.04.2024.
+//
+
+#ifndef IoMT_SDK_umbrella_h
+#define IoMT_SDK_umbrella_h
+
+#import LMTPDecoder.h
+#import LKCPlayManager.h
+#import LKCDecodeHeart.h
+#import lame.h
+#endif /* IoMT_SDK_umbrella_h */
