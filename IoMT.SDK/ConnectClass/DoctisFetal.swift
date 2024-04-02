36c2f2d494f1d8f9bea4718a59c0efb9ce4c1b1f
lmdxnmx <nikita021103@mail.ru>
2024-04-02 13:27:52 +0300

    fix

--- IoMT.SDK/ConnectClass/DoctisFetal.swift
+++ IoMT.SDK/ConnectClass/DoctisFetal.swift
@@ -24,7 +24,7 @@ public class DoctisFetal:
     var serial:String = "";
     static var mail:String = "";
     public var peripherals: [DisplayPeripheral] = []
-    var decoder = LMTP
+    var decoder = LMTPDecoder()
     static var itter:Int = 0
     
     internal var rxtxService: CBService?
